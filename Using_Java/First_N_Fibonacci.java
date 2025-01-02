a367b5785a9e43a25114321558e07048d5362ad0
mubashir12-a <ma2625645@gmail.com>
2025-01-02 20:44:34 +0530

    02-01-2025

--- Using_Java/First_N_Fibonacci.java
+++ Using_Java/First_N_Fibonacci.java
@@ -9,18 +9,13 @@ public class First_N_Fibonacci {
         //                 3 + 5 = 8 ...so-on
 
         int First_N_fibbo = 5;
-
-        int num_1 = 0;
-        int num_2 = 1;
     
         int [] arr = new int[First_N_fibbo];
         arr[0] = 0;
         arr[1] = 1;
 
         for(int i = 2; i < First_N_fibbo; i++){
-            arr[i] = num_1 + num_2;
-            num_1 = num_2;
-            num_2 = arr[i];
+            arr[i] = arr[i - 1] + arr[i - 2];
         }
 
         for(int i = 0; i < arr.length; i++){
